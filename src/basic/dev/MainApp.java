f46aa3c5c0789e193921eba0b05c4aafd2766ef4
biaphan118 <biaphan2002@gmail.com>
2021-10-25 00:25:23 -0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,30 @@
+package basic.dev;
+/**
+ * 
+ * @author biaprfo
+ *
+ */
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		/**
+		 * Khai báo biến
+		 */
+		// TODO Auto-generated method stub
+		String firstName = "Phan Van";
+		String lastName = "Nam";
+		int old = 15;
+		float markMath = 9f;
+		boolean passed = true ;
+		
+		System.out.println("Ho va ten:");
+		System.out.println(firstName + "" + lastName );
+		System.out.println("Mark Math:"+ markMath );
+		System.out.println("Passed:" + passed);
+		//Khai báo hằng. Hằng phải viết hoa
+		final float PI = 3.14f; 
+
+	}
+
+}
